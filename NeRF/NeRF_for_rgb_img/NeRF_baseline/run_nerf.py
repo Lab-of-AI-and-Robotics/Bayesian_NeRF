8cf8ae50c04ae9a574dabc285f1e9a9dcabd59f6
minjae-lulu <lmjbsj@unist.ac.kr>
2024-11-19 22:00:26 +0900

    update

--- NeRF/NeRF_for_rgb_img/NeRF_baseline/run_nerf.py
+++ NeRF/NeRF_for_rgb_img/NeRF_baseline/run_nerf.py
@@ -639,11 +639,11 @@ def config_parser():
                         help='frequency of console printout and metric loggin')
     parser.add_argument("--i_img",     type=int, default=100000, 
                         help='frequency of tensorboard image logging')
-    parser.add_argument("--i_weights", type=int, default=500, 
+    parser.add_argument("--i_weights", type=int, default=50000, 
                         help='frequency of weight ckpt saving')
-    parser.add_argument("--i_testset", type=int, default=500, 
+    parser.add_argument("--i_testset", type=int, default=50000, 
                         help='frequency of testset saving')
-    parser.add_argument("--i_video",   type=int, default=500, 
+    parser.add_argument("--i_video",   type=int, default=50000, 
                         help='frequency of render_poses video saving')
     
     ########################################################################################################   
